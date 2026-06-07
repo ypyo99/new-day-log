de470f9d4b879fe069b466377b981ad53ec64bf9
ypyo99 <ypyo99@gmail.com>
2026-06-07 20:23:55 +0900

    Update version to v260607-notice

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -2207,7 +2207,7 @@ export default function MainApp({
           </div>
 
           <div className="mt-6 sm:mt-8 text-center text-[12px] text-gray-400 font-bold tracking-wider">
-            v260606-copy
+            v260607-notice
           </div>
         </div>
       </div>
