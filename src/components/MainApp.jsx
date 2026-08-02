63fbba117aa1da07607156574792d375de256b07
ypyo99 <ypyo99@gmail.com>
2026-08-02 17:04:08 +0900

    chore: 버전 업데이트 (v260802-Leave)

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -3006,7 +3006,7 @@ export default function MainApp({
           </div>
 
           <div className="mt-6 sm:mt-8 text-center text-[12px] text-gray-400 font-bold tracking-wider">
-            v260729
+            v260802-Leave
           </div>
         </div>
       </div>
