159975a98e7072cee8094fede5e27d97cb8dcc9a
ypyo99 <ypyo99@gmail.com>
2026-08-21 14:16:54 +0900

    chore: 버전 업데이트 v260821-safety training

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -3050,7 +3050,7 @@ export default function MainApp({
           </div>
 
           <div className="mt-6 sm:mt-8 text-center text-[12px] text-gray-400 font-bold tracking-wider">
-            v260821-notice management
+            v260821-safety training
           </div>
         </div>
       </div>
